38b21fd6dd4afe34e0ea73ae1c903631acb00733
DiMeglio-Christopher <christopherdimeglio@yahoo.com>
2019-03-07 02:15:21 -0500

    added predictive thread for cpu moves

--- src/TicTacToe_V2.java
+++ src/TicTacToe_V2.java
@@ -2,23 +2,27 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Arrays;
 import java.util.Random;
 import java.util.concurrent.BrokenBarrierException;
 import java.util.concurrent.CyclicBarrier;
 
-public class TicTacToe {
+public class TicTacToe_V2 {
     private static Boolean playAgain = false;
+    private static TicTacToe_V2 x = null;
+    private final JFrame window;
+    private final int[][] board;
+    private final CyclicBarrier control;
     private String turn;
     private String option;
     private int turnNumber;
-    private final JFrame window;
     private JButton[] buttonList;
-    private final int[][] board;
+    private int[] possibleMoves = new int[9];
     private String winner;
-    private final CyclicBarrier control;
-    private static TicTacToe x = null;
+    private int recentlyPressed;
+    private Thread prediction;
 
-    private TicTacToe() {
+    private TicTacToe_V2() {
         turn = "X";
         option = "";
         turnNumber = 0;
@@ -30,20 +34,20 @@ public class TicTacToe {
     }
 
 
-
-    private static TicTacToe getInstance() {
+    private static TicTacToe_V2 getInstance() {
         if (x == null) {
-            x = new TicTacToe();
+            x = new TicTacToe_V2();
         }
         return x;
     }
 
     public static void main(String[] args) throws InterruptedException, BrokenBarrierException {
         do {
-            TicTacToe game = getInstance();
+            TicTacToe_V2 game = getInstance();
             game.window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
             optionPrompt();
             drawBoard();
+            storePossibleMoves();
             game.control.reset();
             congratulations();
         } while (playAgain);
@@ -52,17 +56,17 @@ public class TicTacToe {
     private static void optionPrompt() {
         JPanel panel = new JPanel();
         JButton player = new JButton("vs Player");
-        player.addActionListener(new TicTacToe.MyActionListener());
+        player.addActionListener(new TicTacToe_V2.MyActionListener());
         x.buttonList[9] = player;
         JButton cpu = new JButton("vs CPU");
         cpu.addActionListener(new MyActionListener());
         x.buttonList[10] = cpu;
         panel.add(player);
         panel.add(cpu);
-       x.window.setContentPane(panel);
-       x.window.pack();
-       x.window.setLocationRelativeTo(null);
-       x.window.setVisible(true);
+        x.window.setContentPane(panel);
+        x.window.pack();
+        x.window.setLocationRelativeTo(null);
+        x.window.setVisible(true);
     }
 
     private static void drawBoard() throws InterruptedException, BrokenBarrierException {
@@ -157,72 +161,86 @@ public class TicTacToe {
         if (x.turnNumber == 1 && !x.buttonList[4].isEnabled()) {
             int[] correctMoves = {0, 2, 6, 8};
             x.buttonList[correctMoves[rand.nextInt(3)]].doClick();
-            return;
-        }
-        if (x.buttonList[4].isEnabled()) {
+        } else if (x.buttonList[4].isEnabled()) {
             x.buttonList[4].doClick();
-            return;
+        } else {
+            x.buttonList[x.possibleMoves[x.recentlyPressed]].doClick();
         }
-        int xRowPos = 0;
-        int yRowPos = 0;
-        int xColumnPos = 0;
-        int yColumnPos = 0;
-        int diagPos = 0;
-        int antiDiagPos = 0;
-        int diagSum = 0;
-        int antiDiagSum = 0;
-        int buttonIndex;
-        for (int i = 0; i < 3; i++) {
-            int rowSum = 0;
-            int columnSum = 0;
-            diagSum += x.board[i][i];
-            antiDiagSum += x.board[i][2 - i];
-            for (int j = 0; j < 3; j++) {
-                rowSum += x.board[i][j];
-                columnSum += x.board[j][i];
-                if (x.board[i][j] == 0) {
-                    xRowPos = i;
-                    yRowPos = j;
-                }
-                if (x.board[j][i] == 0) {
-                    xColumnPos = i;
-                    yColumnPos = j;
-                }
-                if (x.board[i][i] == 0) {
-                    diagPos = i;
-                }
-                if (x.board[i][2 - i] == 0) {
-                    antiDiagPos = i;
-                }
-                if (x.turn.equals("O") && j == 2) {
-                    if (Math.abs(diagSum) == 2 && i == 2) {
-                        buttonIndex = (diagPos * 3) + diagPos;
-                        x.buttonList[buttonIndex].doClick();
-                        System.out.println("Stop Diag: " + buttonIndex);
-                    } else if (Math.abs(antiDiagSum) == 2) {
-                        buttonIndex = (antiDiagPos * 3) + (2 - antiDiagPos);
-                        x.buttonList[buttonIndex].doClick();
-                        System.out.println("Stop AntiDiag: " + buttonIndex);
-                    } else if ((Math.abs(rowSum) == 2)) {
-                        buttonIndex = (xRowPos * 3) + yRowPos;
-                        x.buttonList[buttonIndex].doClick();
-                        System.out.println("Stop Row: " + buttonIndex);
-                    } else if ((Math.abs(columnSum) == 2)) {
-                        buttonIndex = (yColumnPos * 3) + xColumnPos;
-                        x.buttonList[buttonIndex].doClick();
-                        System.out.println("Stop Column: " + buttonIndex);
-                    } else if (i >= 2) {
-                        int index;
-                        while (x.turn.equals("O")) {
-                            int[] correctMoves = {1, 3, 5, 7};
-                            index = rand.nextInt(3);
-                            x.buttonList[correctMoves[index]].doClick();
-                        }
-                        System.out.println("Random Clicked");
+    }
+
+    private static void storePossibleMoves() {
+        x.prediction = new Thread(() -> {
+            while (x.winner.equals("")) {
+                int[][] possibleBoard = x.board;
+                for (int k = 0; k < 9; k++) {
+                    try {
+                        Thread.sleep(1);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
                     }
+                    int i1 = k / 3;
+                    int y = k % 3;
+                    int valueHold = possibleBoard[i1][y];
+                    if (x.board[i1][y] != 1) {
+                        possibleBoard[i1][y] = 1;
+                    } else {
+                        continue;
+                    }
+                    int xRowPos = 0;
+                    int yRowPos = 0;
+                    int xColumnPos = 0;
+                    int yColumnPos = 0;
+                    int diagPos = 0;
+                    int antiDiagPos = 0;
+                    int diagSum = 0;
+                    int antiDiagSum = 0;
+                    int buttonIndex;
+                    for (int i = 0; i < 3; i++) {
+                        int rowSum = 0;
+                        int columnSum = 0;
+                        diagSum += possibleBoard[i][i];
+                        antiDiagSum += possibleBoard[i][2 - i];
+                        for (int j = 0; j < 3; j++) {
+                            rowSum += possibleBoard[i][j];
+                            columnSum += possibleBoard[j][i];
+                            if (x.board[i][j] == 0) {
+                                xRowPos = i;
+                                yRowPos = j;
+                            }
+                            if (x.board[j][i] == 0) {
+                                xColumnPos = i;
+                                yColumnPos = j;
+                            }
+                            if (x.board[i][i] == 0) {
+                                diagPos = i;
+                            }
+                            if (x.board[i][2 - i] == 0) {
+                                antiDiagPos = i;
+                            }
+                            if (Math.abs(diagSum) == 2 && i == 2) {
+                                buttonIndex = (diagPos * 3) + diagPos;
+                                x.possibleMoves[k] = buttonIndex;
+                            } else if (Math.abs(antiDiagSum) == 2) {
+                                buttonIndex = (antiDiagPos * 3) + (2 - antiDiagPos);
+                                x.possibleMoves[k] = buttonIndex;
+                            } else if ((Math.abs(rowSum) == 2)) {
+                                buttonIndex = (xRowPos * 3) + yRowPos;
+                                x.possibleMoves[k] = buttonIndex;
+                            } else if ((Math.abs(columnSum) == 2)) {
+                                buttonIndex = (yColumnPos * 3) + xColumnPos;
+                                x.possibleMoves[k] = buttonIndex;
+                            }
+                            //TODO add random edge button presser
+                        }
+                    }
+                    System.out.println(Arrays.deepToString(possibleBoard));
+                    System.out.println(Arrays.toString(x.possibleMoves));
+                    possibleBoard[i1][y] = valueHold;
                 }
+
             }
-        }
+        });
+        x.prediction.start();
     }
 
     static class MyActionListener implements ActionListener {
@@ -247,6 +265,7 @@ public class TicTacToe {
             }
             for (int i = 0; i < 9; i++) {
                 if (button == x.buttonList[i]) {
+                    x.recentlyPressed = i;
                     button.setEnabled(false);
                     button.setText(x.turn);
                     int i1 = i / 3;
