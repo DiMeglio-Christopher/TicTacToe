cfe26996d27a2beffd6d01445e8c1832f11f197d
DiMeglio-Christopher <christopherdimeglio@yahoo.com>
2019-03-07 00:38:09 -0500

    made TicTacToe class based on a single instance

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -6,105 +6,115 @@ import java.util.Random;
 import java.util.concurrent.BrokenBarrierException;
 import java.util.concurrent.CyclicBarrier;
 
-public class TicTacToe_V3 {
+public class TicTacToe {
     private static Boolean playAgain = false;
     private String turn;
     private String option;
     private int turnNumber;
     private final JFrame window;
-    private final JButton[] buttonList;
+    private JButton[] buttonList;
     private final int[][] board;
     private String winner;
     private final CyclicBarrier control;
+    private static TicTacToe x = null;
 
-    private TicTacToe_V3() {
-        this.turn = "X";
-        this.option = "";
-        this.turnNumber = 0;
-        this.window = new JFrame();
-        this.buttonList = new JButton[11];
-        this.board = new int[3][3];
-        this.winner = "";
-        this.control = new CyclicBarrier(2);
+    private TicTacToe() {
+        turn = "X";
+        option = "";
+        turnNumber = 0;
+        window = new JFrame();
+        buttonList = new JButton[11];
+        board = new int[3][3];
+        winner = "";
+        control = new CyclicBarrier(2);
+    }
+
+
+
+    private static TicTacToe getInstance() {
+        if (x == null) {
+            x = new TicTacToe();
+        }
+        return x;
     }
 
     public static void main(String[] args) throws InterruptedException, BrokenBarrierException {
         do {
-            TicTacToe_V3 game = new TicTacToe_V3();
+            TicTacToe game = getInstance();
             game.window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            game.optionPrompt();
-            game.drawBoard();
+            optionPrompt();
+            drawBoard();
             game.control.reset();
-            game.congratulations();
+            congratulations();
         } while (playAgain);
     }
 
-    private void optionPrompt() {
+    private static void optionPrompt() {
         JPanel panel = new JPanel();
         JButton player = new JButton("vs Player");
-        player.addActionListener(new MyActionListener());
-        buttonList[9] = player;
+        player.addActionListener(new TicTacToe.MyActionListener());
+        x.buttonList[9] = player;
         JButton cpu = new JButton("vs CPU");
         cpu.addActionListener(new MyActionListener());
-        buttonList[10] = cpu;
+        x.buttonList[10] = cpu;
         panel.add(player);
         panel.add(cpu);
-        window.setContentPane(panel);
-        window.pack();
-        window.setLocationRelativeTo(null);
-        window.setVisible(true);
+       x.window.setContentPane(panel);
+       x.window.pack();
+       x.window.setLocationRelativeTo(null);
+       x.window.setVisible(true);
     }
 
-    private void drawBoard() throws InterruptedException, BrokenBarrierException {
-        control.await();
+    private static void drawBoard() throws InterruptedException, BrokenBarrierException {
+        x.control.await();
         JPanel panel = new JPanel(new GridLayout(3, 3));
         for (int i = 0; i < 9; i++) {
-            buttonList[i] = new JButton();
-            buttonList[i].setPreferredSize(new Dimension(200, 200));
-            buttonList[i].setFocusable(false);
-            buttonList[i].setFont(new Font("Times Roman", Font.BOLD, 140));
-            buttonList[i].addActionListener(new MyActionListener());
-            panel.add(buttonList[i]);
+            x.buttonList[i] = new JButton();
+            x.buttonList[i].setPreferredSize(new Dimension(200, 200));
+            x.buttonList[i].setFocusable(false);
+            x.buttonList[i].setFont(new Font("Times Roman", Font.BOLD, 140));
+            x.buttonList[i].addActionListener(new MyActionListener());
+            panel.add(x.buttonList[i]);
         }
         UIManager.put("Button.disabledText", Color.BLACK);
-        window.setVisible(false);
-        window.setContentPane(panel);
-        window.pack();
-        window.setLocationRelativeTo(null);
-        window.setVisible(true);
+        x.window.setVisible(false);
+        x.window.setContentPane(panel);
+        x.window.pack();
+        x.window.setLocationRelativeTo(null);
+        x.window.setVisible(true);
     }
 
-    private void changeTurn() throws InterruptedException, BrokenBarrierException {
-        turnNumber++;
-        if (turnNumber > 4 && checkWinner()) {
-            winner = turn;
-            control.await();
-        } else if (turnNumber > 8) {
-            winner = "draw";
-            control.await();
+    private static void changeTurn() throws InterruptedException, BrokenBarrierException {
+        x.turnNumber++;
+        if (x.turnNumber > 4 && checkWinner()) {
+            x.winner = x.turn;
+            x.control.await();
+        } else if (x.turnNumber > 8) {
+            x.winner = "draw";
+            x.control.await();
         }
-        if ((turnNumber % 2) != 0) {
-            turn = "O";
+        if ((x.turnNumber % 2) != 0) {
+            x.turn = "O";
         } else {
-            turn = "X";
+            x.turn = "X";
         }
-        if (option.equals("cpu") && winner.equals("") && turn.equals("O")) {
+        if (x.option.equals("cpu") && x.winner.equals("") && x.turn.equals("O")) {
             computer();
         }
     }
 
-    private boolean checkWinner() {
+    private static boolean checkWinner() {
         boolean won = false;
         int diagSum = 0;
         int antiDiagSum = 0;
         for (int i = 0; i < 3; i++) {
             int rowSum = 0;
             int columnSum = 0;
-            diagSum += board[i][i];
-            antiDiagSum += board[i][2 - i];
+            diagSum += x.board[i][i];
+            antiDiagSum += x.board[i][2 - i];
             for (int j = 0; j < 3; j++) {
-                rowSum += board[i][j];
-                columnSum += board[j][i];
+                rowSum += x.board[i][j];
+                columnSum += x.board[j][i];
                 if (j == 2 && (Math.abs(rowSum) == 3 || Math.abs(columnSum) == 3 || Math.abs(diagSum) == 3
                         || Math.abs(antiDiagSum) == 3))
                     won = true;
@@ -113,16 +123,16 @@ public class TicTacToe_V3 {
         return won;
     }
 
-    private void congratulations() throws InterruptedException, BrokenBarrierException {
-        control.await();
+    private static void congratulations() throws InterruptedException, BrokenBarrierException {
+        x.control.await();
         for (int i = 0; i < 9; i++) {
-            buttonList[i].setEnabled(false);
+            x.buttonList[i].setEnabled(false);
         }
         JFrame message = new JFrame();
         JPanel panel = new JPanel();
         JLabel label;
-        if (!winner.equals("draw")) {
-            label = new JLabel(winner + " Wins!");
+        if (!x.winner.equals("draw")) {
+            label = new JLabel(x.winner + " Wins!");
         } else {
             label = new JLabel("Draw");
         }
@@ -138,19 +148,19 @@ public class TicTacToe_V3 {
         message.setLocationRelativeTo(null);
         message.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         message.setVisible(true);
-        control.await();
+        x.control.await();
         message.dispose();
     }
 
-    private void computer() throws InterruptedException {
+    private static void computer() {
         Random rand = new Random();
-        if (turnNumber == 1 && !buttonList[4].isEnabled()) {
+        if (x.turnNumber == 1 && !x.buttonList[4].isEnabled()) {
             int[] correctMoves = {0, 2, 6, 8};
-            buttonList[correctMoves[rand.nextInt(3)]].doClick();
+            x.buttonList[correctMoves[rand.nextInt(3)]].doClick();
             return;
         }
-        if (buttonList[4].isEnabled()) {
-            buttonList[4].doClick();
+        if (x.buttonList[4].isEnabled()) {
+            x.buttonList[4].doClick();
             return;
         }
         int xRowPos = 0;
@@ -165,48 +175,48 @@ public class TicTacToe_V3 {
         for (int i = 0; i < 3; i++) {
             int rowSum = 0;
             int columnSum = 0;
-            diagSum += board[i][i];
-            antiDiagSum += board[i][2 - i];
+            diagSum += x.board[i][i];
+            antiDiagSum += x.board[i][2 - i];
             for (int j = 0; j < 3; j++) {
-                rowSum += board[i][j];
-                columnSum += board[j][i];
-                if (board[i][j] == 0) {
+                rowSum += x.board[i][j];
+                columnSum += x.board[j][i];
+                if (x.board[i][j] == 0) {
                     xRowPos = i;
                     yRowPos = j;
                 }
-                if (board[j][i] == 0) {
+                if (x.board[j][i] == 0) {
                     xColumnPos = i;
                     yColumnPos = j;
                 }
-                if (board[i][i] == 0) {
+                if (x.board[i][i] == 0) {
                     diagPos = i;
                 }
-                if (board[i][2 - i] == 0) {
+                if (x.board[i][2 - i] == 0) {
                     antiDiagPos = i;
                 }
-                if (turn.equals("O") && j == 2) {
+                if (x.turn.equals("O") && j == 2) {
                     if (Math.abs(diagSum) == 2 && i == 2) {
                         buttonIndex = (diagPos * 3) + diagPos;
-                        buttonList[buttonIndex].doClick();
+                        x.buttonList[buttonIndex].doClick();
                         System.out.println("Stop Diag: " + buttonIndex);
                     } else if (Math.abs(antiDiagSum) == 2) {
                         buttonIndex = (antiDiagPos * 3) + (2 - antiDiagPos);
-                        buttonList[buttonIndex].doClick();
+                        x.buttonList[buttonIndex].doClick();
                         System.out.println("Stop AntiDiag: " + buttonIndex);
                     } else if ((Math.abs(rowSum) == 2)) {
                         buttonIndex = (xRowPos * 3) + yRowPos;
-                        buttonList[buttonIndex].doClick();
+                        x.buttonList[buttonIndex].doClick();
                         System.out.println("Stop Row: " + buttonIndex);
                     } else if ((Math.abs(columnSum) == 2)) {
                         buttonIndex = (yColumnPos * 3) + xColumnPos;
-                        buttonList[buttonIndex].doClick();
+                        x.buttonList[buttonIndex].doClick();
                         System.out.println("Stop Column: " + buttonIndex);
                     } else if (i >= 2) {
                         int index;
-                        while (turn.equals("O")) {
+                        while (x.turn.equals("O")) {
                             int[] correctMoves = {1, 3, 5, 7};
                             index = rand.nextInt(3);
-                            buttonList[correctMoves[index]].doClick();
+                            x.buttonList[correctMoves[index]].doClick();
                         }
                         System.out.println("Random Clicked");
                     }
@@ -215,33 +225,33 @@ public class TicTacToe_V3 {
         }
     }
 
-    class MyActionListener implements ActionListener {
+    static class MyActionListener implements ActionListener {
         @Override
         public void actionPerformed(ActionEvent e) {
             JButton button = (JButton) e.getSource();
-            if (button == buttonList[9]) {
-                option = "player";
+            if (button == x.buttonList[9]) {
+                x.option = "player";
                 try {
-                    control.await();
+                    x.control.await();
                 } catch (InterruptedException | BrokenBarrierException e1) {
                     e1.printStackTrace();
                 }
             }
-            if (button == buttonList[10]) {
-                option = "cpu";
+            if (button == x.buttonList[10]) {
+                x.option = "cpu";
                 try {
-                    control.await();
+                    x.control.await();
                 } catch (InterruptedException | BrokenBarrierException e1) {
                     e1.printStackTrace();
                 }
             }
             for (int i = 0; i < 9; i++) {
-                if (button == buttonList[i]) {
+                if (button == x.buttonList[i]) {
                     button.setEnabled(false);
-                    button.setText(turn);
-                    int x = i / 3;
+                    button.setText(x.turn);
+                    int i1 = i / 3;
                     int y = i % 3;
-                    board[x][y] = turn.equals("X") ? 1 : -1;
+                    x.board[i1][y] = x.turn.equals("X") ? 1 : -1;
                     try {
                         changeTurn();
                     } catch (InterruptedException | BrokenBarrierException e1) {
@@ -251,13 +261,14 @@ public class TicTacToe_V3 {
             }
             if (button.getText().equals("Play Again")) {
                 playAgain = true;
-                window.dispose();
+                x.window.dispose();
                 try {
-                    control.await();
+                    x.control.await();
                 } catch (InterruptedException | BrokenBarrierException e1) {
                     e1.printStackTrace();
                 }
-                control.reset();
+                x.control.reset();
+                x = null;
             }
         }
     }
